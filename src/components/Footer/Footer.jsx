2e4e1e1727a9a882b7b44568679dab25def5819c
hiep1997xz <hiep1997xz@gmail.com>
2022-10-25 18:22:45 +0700

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Col, Container, Row } from 'reactstrap'
+import logo from '../../assets/images/eco-logo.png'
+import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="logo">
+              <img src={logo} alt="logo" />
+              <div>
+                <h1>Multimart</h1>
+                {/* <p>Since 1995</p> */}
+              </div>
+            </div>
+          </Col>
+          <Col lg="3"></Col>
+          <Col lg="2"></Col>
+          <Col lg="3"></Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
 
