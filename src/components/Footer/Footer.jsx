1faa2eabe3c0a10c16dc73b5fc8fe9726abb0261
hiep1997xz <hiep1997xz@gmail.com>
2022-10-26 10:53:29 +0700

    reponsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,25 +1,100 @@
 import React from 'react'
-import { Col, Container, Row } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import { Col, Container, ListGroup, ListGroupItem, Row } from 'reactstrap'
 import logo from '../../assets/images/eco-logo.png'
 import './Footer.css'
 
 const Footer = () => {
+  const year = new Date().getFullYear()
+
   return (
     <footer className="footer">
       <Container>
         <Row>
-          <Col lg="4">
-            <div className="logo">
-              <img src={logo} alt="logo" />
+          <Col lg="4" className='mb-4'>
+            <div>
               <div>
-                <h1>Multimart</h1>
-                {/* <p>Since 1995</p> */}
+                <h1 className='text-white'>Multimart</h1>
               </div>
+              <p className="footer__text mt-4">
+                In this practical react js project tutorial, I'm going to teach
+                you, step-by-step, how to create a full stack react ecommerce
+                website using reactjs, redux toolkit and firebase v9 from
+                scratch with modern UI and UX
+              </p>
             </div>
           </Col>
-          <Col lg="3"></Col>
-          <Col lg="2"></Col>
-          <Col lg="3"></Col>
+
+          <Col lg="3" className='mb-4'>
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Top Categories</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Mobile Phones</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Modern Sofa</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Arm Chair</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Smart Watches</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="2" className='mb-4'>
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Useful Links</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/shop">Shop</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/cart">Cart</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/login">Login</Link>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3" className='mb-4'>
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Contact</h4>
+              <ListGroup className='footer__contact'>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-line"></i>
+                  </span>
+                  <p>123 Abc, HN, VN</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-phone-line"></i>
+                  </span>
+                  <p>+01234567889</p>
+                </ListGroupItem>
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line"></i>
+                  </span>
+                  <p>example@gmail.com</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="12">
+            <p className="footer__copyright">Copyright {year} developed by VN</p>
+          </Col>
+
         </Row>
       </Container>
     </footer>
